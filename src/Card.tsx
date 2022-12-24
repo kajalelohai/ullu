2bd29a84b14e67183129d0a8811690189d1736c0
Kajal Negi <kl.negi03@gmail.com>
2022-12-24 13:50:43 +0100

    feat: apply styles:

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,11 +1,17 @@
 import { WORDBOOK } from './data';
+import styles from './Card.module.css';
 
 export function Card() {
   return (
-    <div>
+    <div className={styles.card}>
       {WORDBOOK.map((frag) => (
-        <span>{frag.word}</span>
+        <span className={styles.cardText}>{frag.word}</span>
       ))}
+      <div className={styles.footer}>
+        <button className={styles.footerButtons}>Easy</button>
+        <button className={styles.footerButtons}>Normal</button>
+        <button className={styles.footerButtons}>Hard</button>
+      </div>
     </div>
   );
 }
